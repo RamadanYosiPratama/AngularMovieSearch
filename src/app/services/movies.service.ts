6225d2c158271b383be3abc0127cdce3cd207dd5
RamadanYosiPratama <ramadanyosi30@gmail.com>
2020-01-15 11:41:43 +0700

    Angular

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const baseUrl: string = "http://www.omdbapi.com/?apikey=177b1d8d";
+
+@Injectable({
+  providedIn: "root"
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+  searchMovies(searchTerm: string): Observable<any> {
+    return this.http.get(baseUrl, { params: { s: searchTerm } });
+  }
+
+  getMovieDetails(imdbID: string): Observable<any> {
+    return this.http.get(baseUrl, { params: { i: imdbID } });
+  }
+}
